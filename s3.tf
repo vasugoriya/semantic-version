2ca9f1d67c0ba710570a0ee747ac253e849c6268
Your Name <you@example.com>
2024-06-10 17:46:41 +0530

    removed unwanted files

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucketclala12313"
+  bucket = "my-tf-test-bucketclala123131"
 
   tags = {
     Name        = "My bucket"

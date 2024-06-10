f2aa1fcb839056c388f8626f30b1b80b482cc2e9
Your Name <you@example.com>
2024-06-10 18:43:55 +0530

    testing merge

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucketclala123131-test3"
+  bucket = "my-tf-test-bucketclala123131-test4"
 
   tags = {
     Name        = "My bucket"
